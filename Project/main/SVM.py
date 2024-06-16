9682a57a14b5eea3a3df8b58e4ef1615f388aff6
JeonYuchan <eukkchan03@gmail.com>
2024-06-16 13:39:31 +0900

    Update SVM.py

--- Project/main/SVM.py
+++ Project/main/SVM.py
@@ -5,24 +5,26 @@ from sklearn.decomposition import PCA
 import librosa
 import seaborn as sns
 
+
 # 오디오 파일을 로드하고 MFCC 특성 추출
 def load_audio(file_path):
     audio, sr = librosa.load(file_path, sr=None)  # 오디오 파일 로드
     mfcc = librosa.feature.mfcc(audio, sr, n_mfcc=108, hop_length=256)  # MFCC 특성 추출
     return mfcc.T
 
+
 # 오디오 파일 경로 목록
 file_paths = [
-    'wavfile/규보_[cut_8sec].wav',
-    'wavfile/아빠_[cut_8sec].wav',
-    'wavfile/엄마_[cut_8sec].wav',
-    'wavfile/유성_[cut_8sec].wav',
-    'wavfile/유찬_[cut_8sec].wav',
-    'wavfile/이모_[cut_8sec].wav',
-    'wavfile/지윤_[cut_8sec].wav',
-    'wavfile/창희_[cut_8sec].wav',
-    'wavfile/할머니_[cut_8sec].wav',
-    'wavfile/형_[cut_8sec].wav'
+    "wavfile4/규보_[cut_8sec].wav",
+    "wavfile4/아빠_[cut_8sec].wav",
+    "wavfile4/엄마_[cut_8sec].wav",
+    "wavfile4/유성_[cut_8sec].wav",
+    "wavfile4/유찬_[cut_8sec].wav",
+    "wavfile4/이모_[cut_8sec].wav",
+    "wavfile4/지윤_[cut_8sec].wav",
+    "wavfile4/창희_[cut_8sec].wav",
+    "wavfile4/할머니_[cut_8sec].wav",
+    "wavfile4/형_[cut_8sec].wav",
 ]
 
 all_features = []  # 모든 MFCC 특성들을 저장할 리스트
@@ -38,21 +40,21 @@ all_features = np.vstack(all_features)  # 모든 MFCC 특성을 하나의 배열
 labels = np.array(labels)  # 라벨을 배열로 변환
 
 # SVM 모델 초기화 및 학습
-svm_model = svm.SVC(gamma='scale', probability=True)
+svm_model = svm.SVC(gamma="scale", probability=True)
 svm_model.fit(all_features, labels)
 
 # 테스트 음성 파일 경로 목록
 test_file_paths = [
-    'wavfile/규보_[cut_3sec].wav',
-    'wavfile/아빠_[cut_3sec].wav',
-    'wavfile/엄마_[cut_3sec].wav',
-    'wavfile/유성_[cut_3sec].wav',
-    'wavfile/유찬_[cut_3sec].wav',
-    'wavfile/이모_[cut_3sec].wav',
-    'wavfile/지윤_[cut_3sec].wav',
-    'wavfile/창희_[cut_3sec].wav',
-    'wavfile/할머니_[cut_3sec].wav',
-    'wavfile/형_[cut_3sec].wav'
+    "wavfile4/규보_[cut_3sec].wav",
+    "wavfile4/아빠_[cut_3sec].wav",
+    "wavfile4/엄마_[cut_3sec].wav",
+    "wavfile4/유성_[cut_3sec].wav",
+    "wavfile4/유찬_[cut_3sec].wav",
+    "wavfile4/이모_[cut_3sec].wav",
+    "wavfile4/지윤_[cut_3sec].wav",
+    "wavfile4/창희_[cut_3sec].wav",
+    "wavfile4/할머니_[cut_3sec].wav",
+    "wavfile4/형_[cut_3sec].wav",
 ]
 
 # 혼돈 행렬 초기화
@@ -70,21 +72,28 @@ for y_test, test_file_path in enumerate(test_file_paths):
             correct_predictions += 1
     accuracy = correct_predictions / len(test_features) * 100
     individual_accuracies.append(accuracy)
-    print(f'Accuracy for Model {y_test + 1} with Test {y_test + 1}: {accuracy:.2f}%')
+    print(f"Accuracy for Model {y_test + 1} with Test {y_test + 1}: {accuracy:.2f}%")
 
 print(conf)
 
 # 전체 정확률 측정하고 출력
 no_correct = np.trace(conf)
 overall_accuracy = no_correct / np.sum(conf) * 100
-print(f'Overall Accuracy: {overall_accuracy:.2f}%')
+print(f"Overall Accuracy: {overall_accuracy:.2f}%")
 
 # 혼돈 행렬 시각화
 plt.figure(figsize=(10, 8))
-sns.heatmap(conf, annot=True, fmt='g', cmap='Blues', xticklabels=[f'Test {i+1}' for i in range(10)], yticklabels=[f'Model {i+1}' for i in range(10)])
-plt.xlabel('Actual Label')
-plt.ylabel('Predicted Label')
-plt.title('Confusion Matrix')
+sns.heatmap(
+    conf,
+    annot=True,
+    fmt="g",
+    cmap="Blues",
+    xticklabels=[f"Test {i+1}" for i in range(10)],
+    yticklabels=[f"Model {i+1}" for i in range(10)],
+)
+plt.xlabel("Actual Label")
+plt.ylabel("Predicted Label")
+plt.title("Confusion Matrix")
 plt.show()
 
 # 시각화를 위해 차원 축소
@@ -98,16 +107,32 @@ axs = axs.ravel()  # 2D 배열을 1D 배열로 변환
 # 각 테스트 파일과 모델을 서브플롯으로 시각화
 for y_test, (test_file_path, ax) in enumerate(zip(test_file_paths, axs)):
     test_features = load_audio(test_file_path)
-    test_features_pca = pca.transform(test_features)  # 테스트 파일의 특성도 동일한 PCA 변환 적용
-    
+    test_features_pca = pca.transform(
+        test_features
+    )  # 테스트 파일의 특성도 동일한 PCA 변환 적용
+
     model_features_pca = features_pca[labels == y_test]  # 해당 모델의 특성만 선택
 
-    ax.scatter(model_features_pca[:, 0], model_features_pca[:, 1], label=f'Model {y_test+1}', alpha=0.5)
-    ax.scatter(test_features_pca[:, 0], test_features_pca[:, 1], color='black', marker='x', label=f'Test {y_test+1}', alpha=0.8)
+    ax.scatter(
+        model_features_pca[:, 0],
+        model_features_pca[:, 1],
+        label=f"Model {y_test+1}",
+        alpha=0.5,
+    )
+    ax.scatter(
+        test_features_pca[:, 0],
+        test_features_pca[:, 1],
+        color="black",
+        marker="x",
+        label=f"Test {y_test+1}",
+        alpha=0.8,
+    )
 
-    ax.set_xlabel('x')  # x축 레이블
-    ax.set_ylabel('y')  # y축 레이블
-    ax.set_title(f'Model {y_test+1} + Test {y_test+1}\nAccuracy: {individual_accuracies[y_test]:.2f}%')  # 서브플롯 제목
+    ax.set_xlabel("x")  # x축 레이블
+    ax.set_ylabel("y")  # y축 레이블
+    ax.set_title(
+        f"Model {y_test+1} + Test {y_test+1}\nAccuracy: {individual_accuracies[y_test]:.2f}%"
+    )  # 서브플롯 제목
     ax.legend()  # 범례 추가
 
 plt.tight_layout()  # 서브플롯 간의 레이아웃 조정
